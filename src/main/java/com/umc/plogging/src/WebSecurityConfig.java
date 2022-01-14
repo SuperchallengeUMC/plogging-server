e78583ec6c29ff94d5317363741871cdb25d4e53
wjdanfu <wjdanfu@gmail.com>
2022-01-14 14:58:14 +0900

    fix first setting

--- src/main/java/com/umc/plogging/src/WebSecurityConfig.java
+++ src/main/java/com/umc/plogging/src/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.umc.courtking.src;
+package com.umc.plogging.src;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
